5e1fc84f806223e4033d3571d5486811e4d953d7
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-08-16 11:02:20 +0200

    [REACTOR] Change TS autoloading strategy
    Resolves #2

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,28 +1,43 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 # No cachable plugin
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	$_EXTKEY,
-	'Pi1',
-	array(
-		'WebService' => 'display',
-	),
-	// non-cacheable actions
-	array(
-		'WebService' => 'display',
-	)
+    'web_service',
+    'Pi1',
+    array(
+        'WebService' => 'display',
+    ),
+    // non-cacheable actions
+    array(
+        'WebService' => 'display',
+    )
 );
 
 # Cachable plugin
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	$_EXTKEY,
-	'Pi2',
-	array(
-		'WebService' => 'display',
-	)
+    'web_service',
+    'Pi2',
+    array(
+        'WebService' => 'display',
+    )
 );
 
-?>
+// Define whether to automatically load TS.
+$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['web_service']);
+if (false === isset($configuration['autoload_typoscript']) || true === (bool)$configuration['autoload_typoscript']) {
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'web_service',
+        'constants',
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:web_service/Configuration/TypoScript/constants.txt">'
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'web_service',
+        'setup',
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:web_service/Configuration/TypoScript/setup.txt">'
+    );
+}
--- ext_tables.php
+++ ext_tables.php
@@ -1,13 +1,13 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 if (TYPO3_MODE === 'BE') {
 
-	/**
-	 * Registers a Backend Module
-	 */
+    /**
+     * Registers a Backend Module
+     */
 //	Tx_Extbase_Utility_Extension::registerModule(
 //		$_EXTKEY,
 //		'user',	 // Make module a submodule of 'user'
@@ -27,5 +27,8 @@ if (TYPO3_MODE === 'BE') {
 
 }
 
-
-?>
+// Possible Static TS loading
+$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['web_service']);
+if (true === isset($configuration['autoload_typoscript']['value']) && true === (bool)$configuration['autoload_typoscript']['value']) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('web_service', 'Configuration/TypoScript', 'Web Service: expose content in various formats');
+}
