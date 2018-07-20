7980be4613bc22593367a2f7bfded2324d9bdfb3
Fabien Udriot <fabien@omic.ch>
2018-07-20 15:19:07 +0200

    Release 0.2.0 - https://github.com/fabarea/web_service/compare/0.1.0...0.2.0
    Compatibility TYPO3 v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'beta',
-    'version' => '0.2.0-dev',
+    'version' => '0.2.0',
     'autoload' => [
         'psr-4' => ['Fab\\WebService\\' => 'Classes']
     ],
@@ -15,8 +15,8 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '7.6.0-7.6.99',
-                    'vidi' => '',
+                    'typo3' => '7.6.0-8.7.99',
+                    'vidi' => '0.0.0-0.0.0',
                 ],
         ],
 ];
